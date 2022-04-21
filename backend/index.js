82c206146d6e5df3e1b75413ace9d7900e8de498
Harshit Gupta <32036923+harshit2508@users.noreply.github.com>
2022-04-21 14:45:54 +0530

    path added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const port = 8080
+const path = require('path')
 const countryDataRouter = require('./routes/countryDataController')
 
 const cors=require("cors");
